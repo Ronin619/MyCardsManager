d94700bd2f691fd5e2c8b0ecb1002e8c4ba9d834
Ronin619 <eytam619@gmail.com>
2024-02-18 16:20:34 -0500

    update: createCard value to marketValue

--- backend/src/controllers/cardController.js
+++ backend/src/controllers/cardController.js
@@ -1,8 +1,15 @@
 const cardModel = require("../models/cardModel");
 
+//GET: Get all cards
+const findAllCards = async (req, res) => {
+  const allCards = await cardModel.find({});
+
+  res.status(200).json(allCards);
+};
+
 //POST: post new card
 const createCard = async (req, res) => {
-  const { name, set, cardNumber, quantity, value } = req.body;
+  const { name, set, cardNumber, quantity, marketValue } = req.body;
   try {
     const newCard = new cardModel({
       name,
@@ -18,11 +25,19 @@ const createCard = async (req, res) => {
   }
 };
 
-//GET: Get all cards
-const findAllCards = async (req, res) => {
-  const allCards = await cardModel.find({});
+// Delete a card
+const deleteCard = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await cardModel.findByIdAndDelete(id);
 
-  res.status(200).json(allCards);
+    if (!result) {
+      return res.status(404).json({ message: "The card does not exist" });
+    }
+    return res.status(200).send({ message: "Card successfully deleted." });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 };
 
-module.exports = { createCard, findAllCards };
+module.exports = { createCard, findAllCards, deleteCard };
