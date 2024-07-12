06dfb4560c4c64abc93e91ad4866e82c67021613
Ronin619 <eytam619@gmail.com>
2024-07-11 21:08:27 -0400

    update: login form details

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -4,24 +4,35 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <form>
-      <label htmlFor="email">email</label>
-      <input
-        type="email"
-        placeholder="youremail@gmail.com"
-        id="email"
-        name="email"
-      />
-      <label htmlFor="password">password</label>
-      <input
-        type="password"
-        placeholder="********"
-        id="password"
-        name="password"
-      />
-      <button>Login In</button>
-    </form>
+    <>
+      <h2>Login</h2>
+      <form className="login" onSubmit={handleSubmit}>
+        <label htmlFor="email">email</label>
+        <input
+          type="email"
+          placeholder="youremail@gmail.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          placeholder="********"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Login In</button>
+      </form>
+      <p>
+        Don&apos;t have an account? <a href="/signup">sign up</a>
+      </p>
+    </>
   );
 };
 
