600db56fa7bc16c279bf32327f95900880ec0d9c
Ronin619 <eytam619@gmail.com>
2024-07-19 21:26:11 -0400

    feat: implement add card button on inventory page

--- frontend/src/pages/Inventory.jsx
+++ frontend/src/pages/Inventory.jsx
@@ -1,5 +1,12 @@
+import Button from "../components/Button";
+
 const Inventory = () => {
-  return <h1>Welcome!</h1>;
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <Button text="Add Cards" />
+    </>
+  );
 };
 
 export default Inventory;
