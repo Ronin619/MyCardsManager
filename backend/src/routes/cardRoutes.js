1478243b48f6898e819716c8cb5d52da5c052c25
Ronin619 <eytam619@gmail.com>
2024-07-23 21:59:53 -0400

    update: add response cookie settings to login

--- backend/src/routes/cardRoutes.js
+++ backend/src/routes/cardRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cardController = require("../controllers/cardController");
 
 router.get("/", cardController.findAllUsersCards);
-router.post("/", cardController.createCard);
+router.post("/newCard", cardController.createCard);
 router.delete("/:id", cardController.deleteCard);
 router.put("/:id", cardController.editCard);
 
