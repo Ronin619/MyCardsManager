62221e5dce6e2d638af7c016ab2b9a505d982daf
Ronin619 <eytam619@gmail.com>
2024-07-26 23:31:06 -0400

    update: logout button component

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -1,8 +1,24 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 
 const LogoutButton = () => {
   const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        "https://localhost:8080/logout/logoutUser",
+        {},
+        { withCredentials: true }
+      );
+      navigate("/home");
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  };
+
+  return <Button text="Logout" onClick={handleLogout} />;
 };
 
 export default LogoutButton;
