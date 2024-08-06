27921f8936702a99b549da8ac058a6a1a64b06ba
Ronin619 <eytam619@gmail.com>
2024-08-05 21:37:12 -0400

    update: cards table and css

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,29 +1,33 @@
+import "../css/table.css";
+
 const Table = () => {
   return (
-    <div>
-      <table>
-        <tr>
-          <th>Set</th>
-          <th>Name</th>
-          <th>Card Number</th>
-          <th>Quantity</th>
-          <th>Market Value</th>
-          <th>Action</th>
-        </tr>
+    <div className="card-table-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Set</th>
+            <th>Name</th>
+            <th>Card Number</th>
+            <th>Quantity</th>
+            <th>Market Value</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>S&V</td>
+            <td>Pikachu</td>
+            <td>25</td>
+            <td>1</td>
+            <td>$3.00</td>
+            <td className="actions">
+              <ion-icon name="pencil"></ion-icon>
+              <ion-icon name="trash"></ion-icon>
+            </td>
+          </tr>
+        </tbody>
       </table>
-      <tbody>
-        <tr>
-          <td>S&V</td>
-          <td>Pikachu</td>
-          <td>25</td>
-          <td>1</td>
-          <td>$3.00</td>
-          <td>
-            <ion-icon name="create"></ion-icon>
-            <ion-icon name="trash"></ion-icon>
-          </td>
-        </tr>
-      </tbody>
     </div>
   );
 };
