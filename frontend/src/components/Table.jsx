971606172fe86266721ed9d79e28b41838b97855
Ronin619 <eytam619@gmail.com>
2024-08-09 21:09:36 -0400

    feat: add fetch and rendering feature of cards data to inventory page

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,6 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "../css/table.css";
 
 const Table = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const cardData = async () => {
+      try {
+        const response = await axios.get(
+          "https://localhost:8080/findAllUsersCards/",
+          {
+            withCredentials: true,
+          }
+        );
+        console.log(response.data);
+        setCards(response.data);
+      } catch (error) {
+        console.error("fetch cards unsucessful", error);
+      }
+    };
+    cardData();
+  }, []);
+
   return (
     <div className="card-table-container">
       <table className="table">
@@ -15,17 +37,19 @@ const Table = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>S&V</td>
-            <td>Pikachu</td>
-            <td>25</td>
-            <td>1</td>
-            <td>$3.00</td>
-            <td className="actions">
-              <ion-icon name="pencil"></ion-icon>
-              <ion-icon name="trash"></ion-icon>
-            </td>
-          </tr>
+          {cards.map((card) => (
+            <tr key={card._id}>
+              <td>{card.set}</td>
+              <td>{card.name}</td>
+              <td>{card.cardNumber}</td>
+              <td>{card.quantity}</td>
+              <td>{card.marketValue}</td>
+              <td className="actions">
+                <ion-icon name="pencil"></ion-icon>
+                <ion-icon name="trash"></ion-icon>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
