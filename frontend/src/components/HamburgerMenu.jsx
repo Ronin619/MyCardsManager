9e1c96abe0145f74c81a9435ca5f557de48f0d04
Ronin619 <eytam619@gmail.com>
2024-08-15 21:57:35 -0400

    feat: implement hamburger menu component

---
+++ frontend/src/components/HamburgerMenu.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Hamburgermenu = () => {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post(
+        "https://localhost:8080/logout/logoutUser",
+        {},
+        { withCredentials: true }
+      );
+      console.log(response.data);
+      navigate("/home");
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={toggleMenu}>
+        <ion-icon name="menu-sharp"></ion-icon>
+      </button>
+      {isOpen ? (
+        <nav>
+          <ul>
+            <li>
+              <a href="#home" onClick={handleLogout}>
+                Logout
+              </a>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Hamburgermenu;
