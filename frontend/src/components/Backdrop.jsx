bc5f317d9c1b911fb0b2acb174750f1143c4022c
Ronin619 <eytam619@gmail.com>
2024-08-15 21:27:27 -0400

    feat: implement modal styling

---
+++ frontend/src/components/Backdrop.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import "../css/backdrop.css"; // Import the CSS file for backdrop styling
+
+const Backdrop = ({ show }) => {
+  if (!show) return null;
+
+  return <div className="backdrop"></div>;
+};
+
+Backdrop.propTypes = {
+  show: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+export default Backdrop;
--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import "../css/modal.css";
+import Backdrop from "./Backdrop";
 
 const Modal = ({ show, onClose, cardDetails, onSave, onUpdate, heading }) => {
   const [name, setName] = useState("");
@@ -54,66 +56,79 @@ const Modal = ({ show, onClose, cardDetails, onSave, onUpdate, heading }) => {
   }
 
   return (
-    <div>
-      <div>
-        <h2>{heading}</h2>
-        <form>
-          <div>
-            <label>Set</label>
-            <input
-              type="text"
-              name="set"
-              value={set}
-              onChange={(e) => setSet(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Card Number</label>
-            <input
-              type="number"
-              name="cardNumber"
-              value={cardNumber}
-              onChange={(e) => setCardNumber(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Quantity</label>
-            <input
-              type="number"
-              name="quantity"
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Market Value</label>
-            <input
-              type="text"
-              name="marketValue"
-              value={marketValue}
-              onChange={(e) => setMarketValue(e.target.value)}
-            />
-          </div>
-          <div>
-            <button type="button" onClick={handleSaveClick}>
-              Save
-            </button>
-            <button type="button" onClick={onClose}>
-              Cancel
-            </button>
-          </div>
-        </form>
+    <>
+      <Backdrop show={show} onClick={onClose} />
+      <div className="modal">
+        <div>
+          <h2>{heading}</h2>
+          <form className="form">
+            <div className="form-labels">
+              <div>
+                <label>Set</label>
+                <input
+                  type="text"
+                  name="set"
+                  value={set}
+                  onChange={(e) => setSet(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>Card Number</label>
+                <input
+                  type="number"
+                  name="cardNumber"
+                  value={cardNumber}
+                  onChange={(e) => setCardNumber(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>Quantity</label>
+                <input
+                  type="number"
+                  name="quantity"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>Market Value</label>
+                <input
+                  type="text"
+                  name="marketValue"
+                  value={`$${marketValue}`}
+                  onChange={(e) => setMarketValue(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="form-btns-layout">
+              <button
+                className="form-btns form-btn-primary"
+                type="button"
+                onClick={handleSaveClick}
+              >
+                Save
+              </button>
+              <button
+                className="form-btns form-btn-secondary"
+                type="button"
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
