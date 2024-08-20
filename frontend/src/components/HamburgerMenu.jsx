afd1c308b87b26e0f678498e7c649196c7277022
Ronin619 <eytam619@gmail.com>
2024-08-19 21:53:54 -0400

    update: hamburger menu css

--- frontend/src/components/HamburgerMenu.jsx
+++ frontend/src/components/HamburgerMenu.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../css/navbar.css";
+import "../css/hamburgerMenu.css";
 
 const Hamburgermenu = () => {
   const navigate = useNavigate();
