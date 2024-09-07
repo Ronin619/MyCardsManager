4a04be95b1e3b08b66e3da2bc6752b022a1b0a87
Ronin619 <eytam619@gmail.com>
2024-09-06 20:41:53 -0400

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 
-export default defineConfig(({ command }) => {
-  const isDev = command === "serve";
-
-  return {
-    server: isDev
-      ? {
-          https: {
-            key: fs.readFileSync("../backend/server.key"),
-            cert: fs.readFileSync("../backend/server.cert"),
-          },
-        }
-      : undefined,
-    plugins: [react()],
-  };
+export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync("../backend/server.key"),
+      cert: fs.readFileSync("../backend/server.cert"),
+    },
+  },
+  plugins: [react()],
 });
