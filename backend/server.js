ab12be6f9b836940c6c6da217ea66276319a1c47
Ronin619 <eytam619@gmail.com>
2024-09-08 18:24:13 -0400

    update server

--- backend/server.js
+++ backend/server.js
@@ -35,17 +35,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://tcgvault.onrender.com");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  next();
-});
-
 // Handle preflight requests
 app.options("*", cors());
 
